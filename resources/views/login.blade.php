6dcc3f5942fc2755bc88f2abfd170f0ca620f5f4
Demian Flury <74318556+DemianFlury@users.noreply.github.com>
2023-03-01 16:08:33 +0100

    add login form

---
+++ resources/views/login.blade.php
@@ -0,0 +1,8 @@
+<form action="?" method="post">
+    <input type="text" name="firstName" id="firstName" placeholder="First Name">
+    <input type="text" name="lastName" id="lastName" placeholder="Last Name">
+    <input type="email" name="email" id="email" placeholder="Email address">
+    <input type="text" name="notes" id="notes" placeholder="Notes">
+
+    <button type="submit">Log In</button>
+</form>
